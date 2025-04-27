ef4e54e518112ec06faab28e544e7feb08c99cc8
Iffatuz Zahra <iffatuzzahra28@gmail.com>
2025-04-27 17:50:25 +0700

    add: menambahkan logo dan ucapan selamat datang

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -10,6 +10,25 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Color(0XFFF1E7E7),
+      body: Form(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset('assets/images/logo.png', height: 80),
+              const SizedBox(height: 15),
+              const Text(
+                'SELAMAT DATANG KEMBALI',
+                style: TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
+              ),
+              
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
