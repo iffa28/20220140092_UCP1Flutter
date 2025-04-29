a63894f91e02045608b5a92bc4773a34e1a5c012
Iffatuz Zahra <iffatuzzahra28@gmail.com>
2025-04-29 21:11:04 +0700

    add: menambahkan konstruktor untuk menerima data dari halaman sebelumnya

--- lib/detail_piket.dart
+++ lib/detail_piket.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 
 class DetailPiketPage extends StatelessWidget {
-
+  final String nama;
+  final String tanggal;
+  final String tugas;
+  
   const DetailPiketPage({super.key,
-
+    required this.nama,
+    required this.tanggal,
+    required this.tugas,
   });
   
 
