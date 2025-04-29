8f3d080f92da20d0db8bda1687c7b5af4333f347
Iffatuz Zahra <iffatuzzahra28@gmail.com>
2025-04-29 21:17:43 +0700

    add: menambahkan local variabel dan konstruktor untuk menerim data dari halaman sebelumnya

---
+++ lib/customer_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CustomerPage extends StatefulWidget {
+  final String nmAkun;
+  final String emailAkun;
+  final String pwAkun;
+  
+  const CustomerPage({super.key,
+  required this.nmAkun,
+  required this.emailAkun, required this.pwAkun
+  });
+
+  @override
+  State<CustomerPage> createState() => _CustomerPageState();
+}
+
+class _CustomerPageState extends State<CustomerPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    
+    
+  }
+}
