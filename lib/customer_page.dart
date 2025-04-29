1846090cfcd6be75475307a7bfc1fb1edf1613ff
Iffatuz Zahra <iffatuzzahra28@gmail.com>
2025-04-29 21:20:38 +0700

    add: menambahkan controller untuk halaman customer

--- lib/customer_page.dart
+++ lib/customer_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ucp1/detail_pelanggan.dart';
 
 class CustomerPage extends StatefulWidget {
   final String nmAkun;
@@ -15,11 +16,17 @@ class CustomerPage extends StatefulWidget {
 }
 
 class _CustomerPageState extends State<CustomerPage> {
+  final TextEditingController namaCustController = TextEditingController();
+  final TextEditingController emailCustController = TextEditingController();
+  final TextEditingController phoneCustController = TextEditingController();
+  final TextEditingController alamatCustController = TextEditingController();
+  final TextEditingController provinsiCustController = TextEditingController();
+  final TextEditingController kodePosCustController = TextEditingController();
+
   List<Map<String, String>> dataPelanggan = [];
 
   @override
   Widget build(BuildContext context) {
-    
     
   }
 }
