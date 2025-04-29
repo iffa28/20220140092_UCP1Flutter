bf18a228b236de951c5148036c1aba0ff80469b5
Iffatuz Zahra <iffatuzzahra28@gmail.com>
2025-04-29 21:49:15 +0700

    add: menambahkan  map untuk menyimpan daftar nama barang dan satuannya

--- lib/barang_page.dart
+++ lib/barang_page.dart
@@ -1,5 +1,7 @@
-
+import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:ucp1/detail_barang.dart';
 
 class BarangPage extends StatefulWidget {
   final String nmAkun;
@@ -21,6 +23,15 @@ class _BarangPageState extends State<BarangPage> {
   final TextEditingController jumlahBrgController = TextEditingController();
 
   List<Map<String, String>> dataBarang = [];
+
+  final Map<String, int> hargaBarangMap = {
+    'Carrier': 700000,
+    'Sleeping Bag': 100000,
+    'Tenda': 200000,
+    'Sepatu': 400000,
+  };
+
+  int totalHarga = 0;
 
   @override
   Widget build(BuildContext context) {
