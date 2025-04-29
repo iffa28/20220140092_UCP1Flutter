e158be8112057e6b7d4c003481e3ab21346d3de0
Iffatuz Zahra <iffatuzzahra28@gmail.com>
2025-04-29 20:27:19 +0700

    add: menambahkan halaman piket dan konstruktor untuk menerima nama user dari halaman sebelumnya

---
+++ lib/piket_page.dart
@@ -0,0 +1,21 @@
+
+import 'package:flutter/material.dart';
+
+class PiketPage extends StatefulWidget {
+  final String namausr;
+  const PiketPage({super.key,
+  required this.namausr
+  });
+
+  @override
+  State<PiketPage> createState() => _PiketPageState();
+}
+
+class _PiketPageState extends State<PiketPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    
+    
+  }
+}
