df7208a7a6855a2d9f4bb55dfbfa0f4f63bf475b
Iffatuz Zahra <iffatuzzahra28@gmail.com>
2025-04-29 21:43:01 +0700

    add: menambahkan halamanbarang dan construktornya untuk parsing data

---
+++ lib/barang_page.dart
@@ -0,0 +1,23 @@
+
+import 'package:flutter/material.dart';
+
+class BarangPage extends StatefulWidget {
+  final String nmAkun;
+  final String emailAkun;
+  final String pwAkun;
+  const BarangPage({super.key,
+  required this.nmAkun,
+  required this.emailAkun, required this.pwAkun
+  });
+
+  @override
+  State<BarangPage> createState() => _BarangPageState();
+}
+
+class _BarangPageState extends State<BarangPage> {
+  
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}
