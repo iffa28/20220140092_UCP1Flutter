b775a4051c08e1351988c6c74e61f295b5e86da7
Iffatuz Zahra <iffatuzzahra28@gmail.com>
2025-04-29 21:14:13 +0700

    add: menambahkan kode untuk menggil data yang sudah di parsing menggunakan '$' untuk stateless

--- lib/detail_piket.dart
+++ lib/detail_piket.dart
@@ -15,7 +15,64 @@ class DetailPiketPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        title: Text('Detail $tugas', style: TextStyle(fontSize: 24)),
+        backgroundColor: const Color(0XFF2D336B),
+        centerTitle: true,
+        foregroundColor: Colors.white,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('$tanggal',
+                  style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                Container(
+                  width: 150,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(25),
+                    color: Color(0XFF2D336B)
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      '$nama',
+                      style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
+                    ),]
+                  ),)
+
+              ],
+            ),
+            const SizedBox(height: 16),
+
+            Container(
+                  width: 500,
+                  height: 60,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Color(0XFF2D336B)
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '$tugas',
+                        style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
+                      ),]
+                    ),
+                  ),)
+          ],
+        ),
+      ),
     );
   }
 }
